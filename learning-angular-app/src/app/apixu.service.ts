2cfcfce8b44790a8d0e104636b9a5ea944663a99
John Angle <jmangle93@gmail.com>
2021-02-05 15:15:55 -0700

    cleanups, key removal

---
+++ learning-angular-app/src/app/apixu.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { keys } from 'keys';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApixuService {
+
+  constructor(private http: HttpClient) { }
+
+  // The Digitalocean tutorial provided the url api.apixu.com, which forwards now to weatherstack -
+  // Its documentation can help fix the discrepancies between the dated tutorial and current weatherstack response format
+  getWeather(location: string): Observable<any> {
+    return this.http.get(
+      'http://api.weatherstack.com/current?access_key=' + keys.WEATHER_API_KEY + 'b&query=' + location
+    );
+  }
+}
